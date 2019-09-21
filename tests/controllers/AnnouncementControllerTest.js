e8273da8fce9733ae26ff2f1cdb8e20910124741
conrado94 <48019268+conrado94@users.noreply.github.com>
2019-09-21 16:10:37 -0500

    listar estudiante de una convocatoria #16

--- tests/controllers/AnnouncementControllerTest.js
+++ tests/controllers/AnnouncementControllerTest.js
@@ -3,9 +3,13 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../../index');
 const ConvocatoriaRepository = require('../../app/repositories/AnnouncementRepository');
+const personAnnouncementRepository = require('../../app/repositories/personAnnouncementRepository');
+
+
 const Helper = require('../Helper');
 
-const API = '/api/convocatorias-ms/convocatorias';
+const API = '/api/convocatorias-ms/convocatoria';
+
 chai.use(chaiHttp);
 
 describe('Announcement CRUD flows', () => {
@@ -74,8 +78,8 @@ describe('Announcement CRUD flows', () => {
   it('find Announcement by filter test', async () => {
     await ConvocatoriaRepository.create([{
       id: 1,
-      fecha_inicio: '2019-08-24 05:00:00',
-      fecha_final: '2019-08-24 05:00:00',
+      fecha_inicio: '2019-08-24T00:00:00.000Z',
+      fecha_final: '2019-08-24T00:00:00.000Z',
       id_empresa: 1,
       tipo_practica: 'horas',
       id_facultad: 1,
@@ -86,8 +90,8 @@ describe('Announcement CRUD flows', () => {
       id_coordinador: 1,
     }, {
       id: 2,
-      fecha_inicio: '2019-08-24 05:00:00',
-      fecha_final: '2019-08-24 05:00:00',
+      fecha_inicio: '2019-08-24T00:00:00.000Z',
+      fecha_final: '2019-08-24T00:00:00.000Z',
       id_empresa: 2,
       tipo_practica: 'objetivo',
       id_facultad: 2,
@@ -193,59 +197,22 @@ describe('Announcement CRUD flows', () => {
       assert.equal(error.status, 404);
     }));
 
-  it('find Announcement closed calls filter empty test', async () => chai
-    .request(app)
-    .get(`${API}/closedcalls?idPrograma=1&Estado=cerrada`)
-    .then(async (response) => {
-      assert.equal(response.status, 204);
-    }));
-
-  it('find Announcement closed calls filter test', async () => {
-    await ConvocatoriaRepository.create([{
-      id: 5,
-      fecha_inicio: '2019-08-24T00:00:00.000Z0',
-      fecha_final: '2019-08-24T00:00:00.000Z',
-      id_empresa: 1,
-      tipo_practica: 'horas',
-      id_facultad: 1,
-      id_programa: 1,
-      id_ciclo: 1,
-      estado: 'activo',
-      numero_estudiantes: 3,
-      id_coordinador: 1,
-    }, {
-      id: 6,
-      fecha_inicio: '2019-08-24T00:00:00.000Z',
-      fecha_final: '2019-08-24T00:00:00.000Z',
-      id_empresa: 2,
-      tipo_practica: 'objetivo',
-      id_facultad: 2,
-      id_programa: 2,
-      id_ciclo: 2,
-      estado: 'cerrada',
-      numero_estudiantes: 4,
-      id_coordinador: 2,
-    }]);
+  it.only('list studentttttttttttt by announcement', async () => {
+    await personAnnouncementRepository.create([{ id: 1, id_estudiante: 1, id_convocatoria: 1 }]);
 
     return chai
       .request(app)
-      .get(`${API}/closedcalls?idPrograma=2&Estado=cerrada`)
+      .get('/api/convocatorias-ms/estudianteConvocatoria/1')
       .then(async (response) => {
         const { body } = response;
-        assert.deepEqual(body[0], {
-          id: 6,
-          fecha_inicio: '2019-08-24T00:00:00.000Z',
-          fecha_final: '2019-08-24T00:00:00.000Z',
-          id_empresa: 2,
-          tipo_practica: 'objetivo',
-          id_facultad: 2,
-          id_programa: 2,
-          id_ciclo: 2,
-          estado: 'cerrada',
-          numero_estudiantes: 4,
-          id_coordinador: 2,
-        });
+        assert.deepEqual(body, { id: 1, id_estudiante: 1, id_convocatoria: 1 });
       });
   });
-});
 
+  it('find student by annoncement not found test', async () => chai
+    .request(app)
+    .get(`${API}/1`)
+    .catch((error) => {
+      assert.equal(error.status, 404);
+    }));
+});
