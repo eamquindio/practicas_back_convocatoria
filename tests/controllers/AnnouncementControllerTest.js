99b9f1c34396ad79649de2891af6a8f19fac6b5d
Daryl110 <daryl.8220@gmail.com>
2019-11-02 10:29:13 -0500

    corrections in test #1 find convocatory

--- tests/controllers/AnnouncementControllerTest.js
+++ tests/controllers/AnnouncementControllerTest.js
@@ -147,8 +147,8 @@ describe('Announcement CRUD flows', () => {
   it('find Announcement by filter test', async () => {
     await ConvocatoriaRepository.create([{
       id: 1,
-      fecha_inicio: '2019-08-24 05:00:00',
-      fecha_final: '2019-08-24 05:00:00',
+      fecha_final: "2019-08-24T00:00:00.000Z",
+      fecha_inicio: "2019-08-24T00:00:00.000Z",
       id_empresa: 1,
       tipo_practica: 'horas',
       id_facultad: 1,
@@ -159,8 +159,8 @@ describe('Announcement CRUD flows', () => {
       id_coordinador: 1,
     }, {
       id: 2,
-      fecha_inicio: '2019-08-24 05:00:00',
-      fecha_final: '2019-08-24 05:00:00',
+      fecha_final: "2019-08-24T00:00:00.000Z",
+      fecha_inicio: "2019-08-24T00:00:00.000Z",
       id_empresa: 2,
       tipo_practica: 'objetivo',
       id_facultad: 2,
