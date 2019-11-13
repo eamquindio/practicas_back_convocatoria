045097e15fef00dadb43c66b6c9bd3a63f9db3f9
Alejandro Hoyos <0670alejo@gmail.com>
2019-11-13 13:20:14 -0500

    eslint corrections

--- app/resources/ConvocatoryMSResource.js
+++ app/resources/ConvocatoryMSResource.js
@@ -1,7 +1,7 @@
 const ConvocatoryMSResource = module.exports;
 const HTTPClient = require('../utils/HTTPClient');
 
-const BASE_URL = `http://34.70.190.6:80/api/convocatoria/`;
+const BASE_URL = 'http://34.70.190.6:80/api/convocatoria/';
 
 ConvocatoryMSResource.init = () => {
   console.log({ BASE_URL, HTTPClient });
@@ -13,6 +13,6 @@ ConvocatoryMSResource.listStudents =
 ConvocatoryMSResource.sendNotification =
 students => HTTPClient.post('http://34.70.190.6/api/notificaciones/send_mail', {
   subject: 'Convocatoria creada',
-  to: 'students',
-  message: 'Una nueva convocatoria ha sido creada'
+  to: students,
+  message: 'Una nueva convocatoria ha sido creada',
 });
